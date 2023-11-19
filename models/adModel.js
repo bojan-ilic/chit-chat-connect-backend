83edd5ada0cecd5dabf0ab7dfe81de9d4c096869
Bojan Ilic <ilic.bojan.bi@gmail.com>
2023-11-19 14:21:39 +0100

    Create the ad model

---
+++ models/adModel.js
@@ -0,0 +1,30 @@
+// Mongoose library enables schema creation, model definition and interaction with the database
+const { Schema, model } = require('mongoose');
+
+/**
+ * Ad Schema
+ * Defines the structure of the advertisements in 'ads' collection in MongoDB.
+ * Represents the properties and format of the advertisements stored in MongoDB 'ads' collection.
+ */
+const AdSchema = new Schema({
+    title: { type: String, required: true }, // Title of the advertisement
+    body: { type: String, required: true }, // Body/content of the advertisement
+    image: { type: String, required: true }, // Image associated with the advertisement
+    price: { type: Number, required: true }, // Price of the advertised product/service
+    userId: { type: Schema.Types.ObjectId, required: true }, // ID of the user who created the advertisement
+    duration: { type: Number, required: true }, // Duration of the advertisement
+    startDate: { type: Date, default: () => new Date().getTime() }, // Start date of the advertisement
+    endDate: { type: Date, default: null }, // End date of the advertisement
+});
+
+/**
+ * AdModel
+ * Mongoose model based on AdSchema, represents the 'ads' collection.
+ * Provides access to CRUD operations for the advertisements in MongoDB.
+ */
+const AdModel = model('ads', AdSchema);
+
+/**
+ * Exports the AdModel to enable its use throughout the application.
+ */
+module.exports = AdModel;
