a3dca13f3b63cf50f399cd8f66852453019e080a
Bojan Ilic <ilic.bojan.bi@gmail.com>
2023-11-29 21:52:27 +0100

    Add user login endpoint to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,25 @@
+// Importing the Router module from Express for handling routes
 const { Router } = require('express');
+
+// Creating a new instance of the Router class
 const router = new Router();
 
-router.post('/register', require('../controllers/authController/register.js'));
+/**
+ * @route 	POST /api/auth/register
+ * @desc 	Register a new user
+ */
+router.post('/register', require('../controllers/authController/register'));
 
+/**
+ * @route	POST /api/auth/login
+ * @desc	Login an existing user
+ */
+router.post('/login', require('../controllers/authController/login'));
+
+/**
+ * Express Router instance.
+ * Responsible for defining and handling API routes related to authentication.
+ * @type {express.Router}
+ * @module auth
+ */
 module.exports = router;
