8c0bf020ac78fead97d061d117f7631a861fb488
Bojan Ilic <ilic.bojan.bi@gmail.com>
2023-12-26 13:13:13 +0100

    Refactor commentController/addComment.js: Implement postId validation, check post existence, and enhance error handling

--- controllers/commentController/addComment.js
+++ controllers/commentController/addComment.js
@@ -1,20 +1,37 @@
 // Import HTTP status codes and messages for response handling
 const { httpStatus } = require('../../config/constants');
 
-// Import the Comment Model module representing the schema and functionalities for comments
-const commentModel = require('../../models/commentModel');
+// Import the CommentModel representing the Mongoose model for comments based on CommentSchema
+const CommentModel = require('../../models/commentModel');
+
+// Import the PostModel representing the Mongoose model for posts based on PostSchema
+const PostModel = require('../../models/postModel');
 
 /**
  * Controller function to add a new comment to the database.
- * Extracts necessary data from the request and creates a new comment using commentModel.
- * @param {Object} req - The request object containing comment details
- * @param {Object} res - The response object used to send success message or error message when adding comment
- * @returns {Object} - Returns a response with the saved comment or an error message
+ * Extracts necessary data from the request and creates a new comment using CommentModel.
+ * @param {Object} req - The request object representing the incoming request and containing comment data for database addition.
+ * @param {Object} res - The response object representing the server's response, used to send success message or error message when adding comment.
+ * @returns {Object} - Returns a response object representing the server's reply containing saved comment data in case of a successful addition or an error message upon an unsuccessful attempt to add the comment to the database.
  */
 const addComment = async (req, res) => {
     try {
-        // Extract user details from token present in request locals
+        // Extract user details (_id, firstName, lastName) from the token present in request locals
         const { _id, firstName, lastName } = req.locals;
+
+        // Extracts the 'postId' from the incoming request body according to the CommentSchema, linking the comment to a particular post
+        const { postId } = req.body;
+
+        // Check if the post with the provided ID exists
+        const postExists = await PostModel.exists({ _id: postId });
+
+        if (!postExists) {
+            return res.status(httpStatus.NOT_FOUND.code).send({
+                status: 'error',
+                message: httpStatus.NOT_FOUND.message,
+                customMessage: 'The referenced post does not exist.',
+            });
+        }
 
         // Construct a new comment object merging request body details with user information from the token
         const comment = {
@@ -28,13 +45,13 @@ const addComment = async (req, res) => {
             },
         };
 
-        // Create a new comment instance using commentModel schema
-        const newComment = new commentModel(comment);
+        // Create a new comment instance using CommentModel schema
+        const newComment = new CommentModel(comment);
 
         // Save the newly created comment to the database
         const savedComment = await newComment.save();
 
-        // Send a success response indicating successful comment addition
+        // Send a success response indicating successful comment addition to the database
         res.status(httpStatus.SUCCESS.code).send({
             status: 'success',
             message: httpStatus.SUCCESS.message,
@@ -42,7 +59,7 @@ const addComment = async (req, res) => {
             data: savedComment, // Include the 'savedComment' object within the 'data' field of the response object
         });
     } catch (error) {
-        // Handling errors and sending appropriate error response
+        // Error handling and sending appropriate error response
         res.status(httpStatus.SERVICE_ERROR.code).send({
             status: 'error',
             message: httpStatus.SERVICE_ERROR.message,
