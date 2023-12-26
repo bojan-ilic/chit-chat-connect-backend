447e9c29bb207b246abcfe5d9d0209fbbe08f878
Bojan Ilic <ilic.bojan.bi@gmail.com>
2023-12-26 16:43:24 +0100

    Refactor commentController/getAllCommentsForPost.js: Improve error handling and response messages for retrieving comments by postId

--- controllers/commentController/getAllCommentsForPost.js
+++ controllers/commentController/getAllCommentsForPost.js
@@ -1,21 +1,22 @@
 // Import HTTP status codes and messages for response handling
 const { httpStatus } = require('../../config/constants');
 
-// Import the CommentModel representing the schema and functionalities for comments
+// Import the CommentModel representing the Mongoose model for comments based on CommentSchema
 const CommentModel = require('../../models/commentModel');
 
 /**
  * Controller function to retrieve all comments for a specific post from the database.
- * @param {Object} req - The request object containing the parameters used for comment.http retrieval.
- * @param {Object} res - The response object used to send the retrieved comments or error messages.
- * @returns {Object} Returns a response with the list of comments or an error message.
+ * Retrieves comments related to a particular post using the postId from the request object.
+ * @param {Object} req - The request object representing the incoming request with post-related parameters for comment retrieval.
+ * @param {Object} res - The response object representing the server's response, used to send fetched comments or error messages related to the request.
+ * @returns {Object} - Returns a response object representing the server's reply containing a list of comments for the post or an error message in case of failure.
  */
 const getAllCommentsForPost = async (req, res) => {
     try {
-        // Destructuring postId from request parameters to retrieve comments related to a specific post
+        // Extract the post ID from the request parameters with the alias 'postId' to retrieve comments related to a specific post
         const { postId } = req.params;
 
-        // Fetch comments only if postId is available
+        // Check if postId is missing, send an error response indicating that the requested post was not found
         if (!postId) {
             return res.status(httpStatus.NOT_FOUND.code).send({
                 status: 'error',
@@ -43,15 +44,24 @@ const getAllCommentsForPost = async (req, res) => {
         // Count of comments retrieved for the specific post
         const count = comments.length;
 
-        // Sending a successful response with comments for the requested post
+        // Handles the case where no comments are found for the specified post
+        if (count === 0) {
+            return res.status(httpStatus.SUCCESS.code).send({
+                status: 'success',
+                message: httpStatus.SUCCESS.message,
+                customMessage: 'No comments found for this post.',
+            });
+        }
+
+        // Send a success response with comments for the requested post
         res.status(httpStatus.SUCCESS.code).send({
             status: 'success',
             message: httpStatus.SUCCESS.message,
             customMessage: 'Comments for the post retrieved successfully.',
-            data: { count, comments }, // Sending data object containing comments and count
+            data: { count, comments }, // Send data object containing comments and count
         });
     } catch (error) {
-        // Handling errors and sending an appropriate error response
+        // Error handling and sending appropriate error response
         res.status(httpStatus.SERVICE_ERROR.code).send({
             status: 'error',
             message: httpStatus.SERVICE_ERROR.message,
