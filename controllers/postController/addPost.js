ccb4396c9af835c7569cbbc0e09e6981e0c36878
Bojan Ilic <ilic.bojan.bi@gmail.com>
2023-12-27 21:06:24 +0100

    Refactor postController/addPost.js: Improve comment clarity and standardize error handling for post addition functionality

--- controllers/postController/addPost.js
+++ controllers/postController/addPost.js
@@ -1,27 +1,25 @@
 // Import HTTP status codes and messages for response handling
 const { httpStatus } = require('../../config/constants');
 
-// Importing the PostModel module representing the schema and functionalities for posts
+// Import PostModel representing the Mongoose model for posts based on PostSchema
 const PostModel = require('../../models/postModel');
 
 /**
  * Controller function to add a new post.
- * @param {Object} req - The request object containing post data.
- * @param {Object} res - The response object for sending the HTTP response.
- * @returns {void}
+ * Extracts required data from the request object and creates a new post using PostModel.
+ * @param {Object} req - The request object representing the incoming request and containing post data for database addition.
+ * @param {Object} res - The response object representing the server's response, used to send success message or error message when adding post.
+ * @returns {Object} - Returns a response object representing the server's reply containing saved post data in case of a successful addition or an error message upon an unsuccessful attempt to add the post to the database.
  */
 const addPost = async (req, res) => {
     try {
-        // Retrieve user information from token present in request locals
-        const userFromToken = req.locals;
-
-        // Extract userId from userFromToken object
-        const { _id: userId } = userFromToken;
+        // Extract user ID from the token representing the logged-in user
+        const { _id: userId } = req.locals;
 
         // Create a new post object by combining request body and userId
         const post = {
-            ...req.body,
-            userId,
+            ...req.body, // Retrieve data from the request body
+            userId, // Add the user ID to the post data
         };
 
         // Create a new instance of PostModel with the post object
@@ -35,7 +33,7 @@ const addPost = async (req, res) => {
             status: 'success',
             message: httpStatus.SUCCESS.message,
             customMessage: 'Post added successfully',
-            data: savedPost,
+            data: savedPost, // Include the post data in the success response
         });
     } catch (error) {
         // Send an error response if there is an issue while saving the post
