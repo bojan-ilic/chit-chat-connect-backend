cca0c2f06671f784a622ad461113068c9b0c7ab6
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-02-09 14:30:28 +0100

    refactor: Migrate messageModel.js to TypeScript, define 'Message' interface for precise document structure definition

--- models/messageModel.ts
+++ models/messageModel.ts
@@ -1,5 +1,19 @@
-// Mongoose library enables schema creation, model definition and interaction with the database
-const { Schema, model } = require('mongoose');
+// Mongoose library enables schema creation, model definition, and interaction with the database (including Model and Document types)
+import {Schema, model, Model, Document} from 'mongoose';
+
+/**
+ * Defines the structure and properties of a message document stored in the MongoDB 'messages' collection.
+ * Extends the Mongoose Document type for enhanced TypeScript support, providing additional functionality and typings
+ * for interacting with MongoDB documents.
+ * @interface Message
+ */
+interface Message extends Document {
+	senderId: Schema.Types.ObjectId; // ID of the message sender
+	receiverId: Schema.Types.ObjectId; // ID of the message receiver
+	createdAt: Date; // Timestamp when the message was created
+	message: string; // Content of the message
+	seenAt?: Date | null; // Timestamp when the message was seen by the receiver (optional)
+}
 
 /**
  * Message Schema
@@ -7,23 +21,26 @@ const { Schema, model } = require('mongoose');
  * Represents the properties and format of messages stored in MongoDB 'messages' collection.
  */
 const MessageSchema = new Schema({
-    senderId: { type: Schema.Types.ObjectId, required: true }, // ID of the message sender
-    receiverId: { type: Schema.Types.ObjectId, required: true }, // ID of the message receiver
-    createdAt: { type: Date, default: () => new Date().getTime() }, // Timestamp when the message was created
-    message: { type: String, required: true }, // Content of the message
-    seenAt: { type: Date, default: null }, // Timestamp when the message was seen by the receiver
+	senderId: {type: Schema.Types.ObjectId, required: true}, // ID of the message sender
+	receiverId: {type: Schema.Types.ObjectId, required: true}, // ID of the message receiver
+	createdAt: {type: Date, default: () => new Date().getTime()}, // Timestamp when the message was created
+	message: {type: String, required: true}, // Content of the message
+	seenAt: {type: Date, default: null} // Timestamp when the message was seen by the receiver
 });
 
 /**
  * MessageModel
  * Mongoose model based on MessageSchema, represents the 'messages' collection.
  * Provides access to CRUD operations for messages in MongoDB.
+ * This Model represents the MongoDB 'messages' collection and is typed as Model<Message & Document>,
+ * where 'Message' is the interface defining the message document structure, and 'Document' is the Mongoose document type.
  * @exports MessageModel
+ * @type {Model<Message & Document>}
  */
-const MessageModel = model('messages', MessageSchema);
+const MessageModel = model<Message>('messages', MessageSchema) as Model<Message & Document>;
 
 /**
  * Exports the MessageModel to enable its use throughout the application.
  * @module messageModel
  */
-module.exports = MessageModel;
+export default MessageModel;
