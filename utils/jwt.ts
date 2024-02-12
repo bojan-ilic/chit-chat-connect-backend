5006a459486290d2e1266ef77034c18ebfa08f72
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-02-12 12:08:19 +0100

    refactor: Migrate JWT handling from JavaScript to TypeScript

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,21 +1,21 @@
-// Importing the 'jsonwebtoken' library for handling JSON Web Tokens (JWT)
-const jwt = require('jsonwebtoken');
+// Import the 'jsonwebtoken' library for handling JSON Web Tokens (JWT)
+import jwt from 'jsonwebtoken';
 
-// Extracting the 'JWT_KEY' constant from the configuration file to facilitate JWT encoding and decoding
-const { JWT_KEY } = require('../config/config');
+// Extract the 'JWT_KEY' constant from the configuration file to facilitate JWT encoding and decoding
+import {JWT_KEY} from '../config/config';
 
 /**
  * createToken function generates a JSON Web Token (JWT) based on the provided payload and expiration parameters.
- * @param {Object} payload - Data to be included in the token.
+ * @param {object} payload - Data to be included in the token.
  * @param {string | number} expiresIn - Expiration period for the token (e.g., '1d' for 1 day).
  * @returns {string} - Generated JWT token.
  */
-const createToken = (payload, expiresIn) => {
-    return jwt.sign(payload, JWT_KEY, { expiresIn });
+const createToken = (payload: object, expiresIn: string | number): string => {
+	return jwt.sign(payload, JWT_KEY, {expiresIn});
 };
 
 /**
  * Exports the 'createToken' function to enable its usage throughout the application.
  * @module createToken
  */
-module.exports = createToken;
+export default createToken;
